b977ee2c4a093e93fb8b125ec10f229dfeedbb7a
miyamotoshin <79813082+miyamotoshin@users.noreply.github.com>
2021-03-20 16:49:17 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('a[href^="#"]').click(function(){
+    let speed = 500;
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+  intersectAction($('.in'), function(element, isIntersecting){
+    if(isIntersecting){
+      element.addClass('on');
+    }
+  })
+});
