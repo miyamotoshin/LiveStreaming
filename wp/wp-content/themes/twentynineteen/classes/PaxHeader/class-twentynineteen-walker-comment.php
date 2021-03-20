65e20e53dd813efb785fe79a66363e4d63d207d9
miyamotoshin <79813082+miyamotoshin@users.noreply.github.com>
2021-03-21 00:45:33 +0900

    Add files via upload

---
+++ wp/wp-content/themes/twentynineteen/classes/PaxHeader/class-twentynineteen-walker-comment.php
@@ -0,0 +1,1 @@
+146 path=20210318_dev環境設置確認用/webコンテンツ/wp/wp-content/themes/twentynineteen/classes/class-twentynineteen-walker-comment.php
---
+++ wp/wp-content/themes/twentynineteen/inc/PaxHeader/color-patterns.php
@@ -0,0 +1,1 @@
+121 path=20210318_dev環境設置確認用/webコンテンツ/wp/wp-content/themes/twentynineteen/inc/color-patterns.php
