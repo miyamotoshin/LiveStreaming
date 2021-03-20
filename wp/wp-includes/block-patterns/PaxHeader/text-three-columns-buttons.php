579a69f346b452dc246ef2d9c0e963d2d60c67df
miyamotoshin <79813082+miyamotoshin@users.noreply.github.com>
2021-03-21 01:44:52 +0900

    Add files via upload

---
+++ wp/wp-includes/block-patterns/PaxHeader/text-three-columns-buttons.php
@@ -0,0 +1,1 @@
+123 path=20210318_dev環境設置確認用/webコンテンツ/wp/wp-includes/block-patterns/text-three-columns-buttons.php
---
+++ wp/wp-includes/block-patterns/PaxHeader/text-two-columns.php
@@ -0,0 +1,1 @@
+113 path=20210318_dev環境設置確認用/webコンテンツ/wp/wp-includes/block-patterns/text-two-columns.php
